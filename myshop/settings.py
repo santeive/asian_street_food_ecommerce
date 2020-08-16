41778c020f4ae8ea793b1ffc571386a3b4151c4b
santeive <packo.ive@gmail.com>
2020-08-15 20:07:08 -0500

    heroku configs

--- myshop/settings.py
+++ myshop/settings.py
@@ -114,10 +114,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+django_heroku.settings(locals())
