5b752f8bfa45a862c40ff449821e11405f0f3dd5
santeive <packo.ive@gmail.com>
2020-08-22 20:27:46 -0500

    detail product complete

--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,10 @@ from cart.forms import CartAddProductForm
 from .models import Category, Product
 
 
+def home(request):
+    categories = Category.objects.all()
+    return render(request, 'shop/product/index.html', {'categories': categories})
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
@@ -10,10 +14,10 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html',
+    return render(request, 'shop/product/list2.html',
                   {'category': category,
-                   'categories': categories,
-                   'products': products})
+                   'products': products,
+                   'categories': categories})
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product,
@@ -23,6 +27,6 @@ def product_detail(request, id, slug):
     cart_product_form = CartAddProductForm()
 
     return render(request,
-                  'shop/product/detail.html',
+                  'shop/product/detail2.html',
                   {'product': product,
                    'cart_product_form': cart_product_form})
