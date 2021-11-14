52d0b85041e173f0976f74bc03deb6cfb3fc50c7
santeive <packo.ive@gmail.com>
2021-11-13 21:22:29 -0600

    Django manual fields added and checkout screen

--- orders/forms.py
+++ orders/forms.py
@@ -11,4 +11,12 @@ class OrderCreateForm(forms.ModelForm):
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 
                   'postal_code', 'city']
-    
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'postal_code': forms.NumberInput(attrs={'class': 'form-control'}),
+            'city': forms.TextInput(attrs={'class': 'form-control'}),
+        }
